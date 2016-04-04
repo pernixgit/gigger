93905fc614a9afe885701b750a7cee4d3391068c
aleks279 <szamora@pernix-solutions.com>
2016-04-04 11:43:23 -0600

    add db constraints

--- db/migrate/20160401165639_create_event_type.rb
+++ db/migrate/20160401165639_create_event_type.rb
@@ -1,7 +1,7 @@
 class CreateEventType < ActiveRecord::Migration
   def change
     create_table :event_types do |t|
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps null: false
     end
--- db/migrate/20160401171705_create_instrument.rb
+++ db/migrate/20160401171705_create_instrument.rb
@@ -1,7 +1,7 @@
 class CreateInstrument < ActiveRecord::Migration
   def change
     create_table :instruments do |t|
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps null: false
     end
