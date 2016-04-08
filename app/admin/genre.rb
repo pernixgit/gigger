e7c7b058b5a63169a25ff918ccf33f6347877b69
Saul Zamora <szamora@pernix-solutions.com>
2016-04-08 10:26:36 -0500

    Add active admin (#9)
    * add ActiveAdmin setup

---
+++ app/admin/genre.rb
@@ -0,0 +1,21 @@
+ActiveAdmin.register Genre do
+  permit_params :name
+
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :created_at
+    actions
+  end
+
+  filter :name
+  filter :created_at
+
+  form do |f|
+    f.inputs "Genre Details" do
+      f.input :name
+    end
+    f.actions
+  end
+end
