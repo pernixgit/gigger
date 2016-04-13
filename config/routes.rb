c674ac6a24a2935102a73f59d13b38daf5780074
aleks279 <szamora@pernix-solutions.com>
2016-04-13 11:53:01 -0600

    fix typo in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controller: { registrations: "users/registrations" }
+  devise_for :users, controllers: { registrations: "users/registrations" }
   
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
