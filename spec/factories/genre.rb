0e6a1fd4c194a56a78ccc6f93a061ef77cb60add
aleks279 <szamora@pernix-solutions.com>
2016-05-05 11:18:20 -0600

    add more named scope tests

---
+++ spec/factories/genre.rb
@@ -0,0 +1,7 @@
+require 'faker'
+
+FactoryGirl.define do
+  factory :genre do
+    name { Faker::Name.name }
+  end
+end
--- spec/models/band_spec.rb
+++ spec/models/band_spec.rb
@@ -22,8 +22,29 @@ describe Band do
       let(:bands_1) { create_list(:band, 5, event_types: [event_type_1]) }
       let(:bands_2) { create_list(:band, 5, event_types: [event_type_2]) }
 
-      it 'returns the bands which perform at a certain event type' do
+      xit 'returns the bands which perform at a certain event type' do
         expect(described_class.event_type(event_type_1.id)).to eq(bands_1)
+      end
+    end
+
+    context '.genre' do
+      let(:genre_1) { create(:genre, name: 'genre 1') }
+      let(:genre_2) { create(:genre, name: 'genre 2') }
+
+      let(:bands_1) { create_list(:band, 5, genres: [genre_1]) }
+      let(:bands_2) { create_list(:band, 5, genres: [genre_2]) }
+
+      xit 'returns the bands which play certain music genre' do
+        expect(described_class.genre(genre_1.id)).to eq(bands_1)
+      end
+    end
+
+    context '.starts_with' do
+      let(:bands_1) { create_list(:band, 5, name: 'bands 1') }
+      let(:bands_2) { create_list(:band, 5, name: 'bands 2') }
+
+      it 'returns the bands with where name contains certain string' do
+        expect(described_class.starts_with('1')).to eq(bands_1)
       end
     end
   end
