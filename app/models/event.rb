47845aef57872289123c30c403ce5b52faa21554
aleks279 <szamora@pernix-solutions.com>
2016-05-05 18:45:11 -0600

    consistency

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ActiveRecord::Base
   include Filterable
 
-  scope :event_type, -> (event_type) { where event_type_id: event_type}
+  scope :event_type, -> (event_type) { where(event_type_id: event_type) }
   scope :starts_with, -> (name) { where('name like ?', "%#{name}%") }
 
   belongs_to :client
